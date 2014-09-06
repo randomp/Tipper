6518843e4b8e6be787508dd6e081632b6f1a9e2a
peiqi <peiqi@yahoo-inc.com>
2014-09-06 15:18:55 -0700

    Initial Commit

---
+++ Tipper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipper
+//
+//  Created by Peiqi Zheng on 9/6/14.
+//  Copyright (c) 2014 Peiqi Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
